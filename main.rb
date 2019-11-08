e8b3f99a3ee810a44c5cd2b5b259c2b5d93ac5ad
Katie <43352205+katie311@users.noreply.github.com>
2019-11-07 21:06:06 -0700

    Add files via upload

--- main.rb
+++ main.rb
@@ -1,58 +1,73 @@
 require "pry"
 
-class Lunch_Lady
-    def initialize
-        @main_dishes = ["Pizza", "Burger", "Chili"]
-        @tray = []
-        puts "Welcome to the lunchroom."
-        puts "Please choose a main dish."
-        add_main
-        puts "Please choose two sides"
-        add_side
+@diner = [
+    {name: '', budget: ''}
+]
+
+@tray = {main: '', side1: '', side2: ''}
+@mains = [
+    { :Pizza => "10", :Burger => "8",
+    :Chili => "5" }
+]
+@sides = [
+    { :Fries => "3", :Fruit => "4", :Salad => "6"}
+]
+def menu
+    puts "-- Welcome to the Lunch Room --"
+    puts "1) Create Profile"
+    puts "2) Select Meal"
+    puts "3) View Tray"
+    puts "4) Exit"
+
+    choice = gets.to_i
+
+    case choice
+    when 1
+        add_diner
+    when 2
+        choose_meal
+    when 3 
+        view_tray
+    when 4 
+        puts "Goodbye"
+        exit
+    else
+        puts "Invalid Choice"
     end
-
-    def add_main
-        @main_dishes.each_with_index do |main_dishes, index|
-            puts "#{index + 1}. #{main_dishes}"
-        end
-        
-        main_choice = gets.to_i
-        if user == 1 
-            @tray << @main_dishes[main_choice - 1]
-        elsif user == 2
-            @tray << 'Burger'
-        else 
-            @tray << 'Chili'
-        end
-
-        # customer = {name: 'test', main: 'test', side: '', wallet: ''}    
-    end
-
+    menu
 end
+
+    def add_diner
+        diner = {}
+        puts "Enter Your Name:"
+        diner[:name] = gets.chomp
+        puts "How much would you like to spend? $___"
+        diner[:budget] = gets.chomp
+        puts "Profile Created Successfully."
+        @diner << diner
+    end
+
+
+    def choose_meal
+        puts "Choose a Main Dish:"
+        puts @mains
+        @tray[:main] = gets.chomp
+        puts "Your Tray:"
+        puts @tray[:main]
+        puts "Choose Side 1"
+        puts @sides
+        @tray[:side1] = gets.chomp
+        puts "Choose Side 2"
+        puts @sides
+        @tray[:side2] = gets.chomp
+        puts "Your Tray:"
+        puts @tray
+    end
     
-
-#         # main_choice
-#     end
-#     main_choice(@main_dishes)
-# def main_choice
-#     puts ">"
-#     choice = gets.to_i - 1 
-#     puts "You have selected #{[choice]}"
-# end
+    def view_tray
+        puts @tray
+    end
 
 
-# def main_choice
-    #     puts ">"
-    #     choice = gets.to_i - 1
-    # end
-    #     puts ">"
-    #     input = gets.to_i - 1
-    #     "#{@mains[input]}"
-    #     puts "Item added to tray."
-    # end
 
-
-Lunch_Lady.new
-
-
-# @tray push item index selection to tray
+menu
